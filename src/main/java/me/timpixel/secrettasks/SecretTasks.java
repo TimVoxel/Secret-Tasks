601ed5fc41e3158f0ce40b3ccc8ce1709810db44
TimPixel <tim.buyalski@gmail>
2024-11-21 17:21:07 +0300

    Initial commit

---
+++ src/main/java/me/timpixel/secrettasks/SecretTasks.java
@@ -0,0 +1,17 @@
+package me.timpixel.secrettasks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SecretTasks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
